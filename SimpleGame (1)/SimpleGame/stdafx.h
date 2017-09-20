d3e191df0f30319b23d38f7828f57fb4ca4d2e9b
JaeHoon <zoo0217@naver.com>
2017-09-20 11:15:51 +0900

    Add files via upload

---
+++ SimpleGame (1)/SimpleGame/stdafx.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "targetver.h"
+
+#include <stdio.h>
+#include <tchar.h>
+
+#include <iostream>
+#include <map>
+#include <vector>
+#include <list>
+#include <algorithm>
+
+using namespace std;
+
